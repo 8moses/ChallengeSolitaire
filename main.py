e6b0523062ced98f20d1779bafe246d50fab26bd
YasmineBch123 <113337455+YasmineBch123@users.noreply.github.com>
2023-01-20 13:36:42 +0100

    new

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import random
 from sys import exit
 # achtergrond
 pygame.init()
@@ -19,38 +18,8 @@ while True:
           
     pygame.display.update()    
     #kaarten
-    CARDWIDTH = 100
-    CARDHEIGHT = 150
-    MARGIN = 10
-    XSPACING = CARDWIDTH + 2*MARGIN
-    YSPACING = CARDHEIGHT + 4*MARGIN
-    OFFSET = 5
 
 
-    HARTEN = 'hart'
-    DIAMANTEN = 'diamant'
-    CLUBS = 'club'
-    SPADES = 'spade'
-
-    RED = 'red'
-    BLACK = 'black'
-    COLOR = {}
-
-    for s in (HARTEN, DIAMANTEN):
-     COLOR [s] = RED
-    for s in (CLUBS, SPADES):
-     COLOR[s] = BLACK
-
-    ALLSUITS = COLOR.keys()
-    NSUITS = len(ALLSUITS)
-    ACE = 1
-    JACK = 11
-    QUEEN = 12
-    KING = 13
-    ALLVALUES = range(1, 14) # (one more than the highest value)
-    NVALUES = len(ALLVALUES)
-    ALNAMES = ["", "A"] + map(str, range(2, 11)) + ["J", "Q", "K"]
-    
 
 
 
