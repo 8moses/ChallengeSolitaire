7d55a09cea3e69fb04072dc85ea5419cd58b6384
8moses <506869@vistacollege.nl>
2023-01-20 13:01:06 +0100

    achtergrond
    achtergrond

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((1200,800))
 pygame.display.set_caption('Runner')
 
-img = pygame.image.load('backgroundgrid.jpg')
+img = pygame.image.load('backgroundimagegrid.jpg')
 
 
 while True:
-    screen.blit(img, (100, 0))
+    screen.blit(img, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
