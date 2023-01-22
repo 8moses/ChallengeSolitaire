ee40c0f388b2e0c27eaf7da94fe27739a6c1ab38
8moses <506869@vistacollege.nl>
2023-01-22 20:59:13 +0100

    update
    update

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 import pygame
+import random
 from sys import exit
-# achtergrond
+from enum import Enum
+
+#scherm
 pygame.init()
-screen = pygame.display.set_mode((1200,800))
+screen = pygame.display.set_mode((1000,800))
 pygame.display.set_caption('Runner')
+font = pygame.font.Font(None, 50)
 
-img = pygame.image.load('backgroundimagegrid.jpg')
+#achtergrond
+achtergrond = pygame.image.load('backgroundimagegrid.jpg')
+Text = font.render('Stage', True, 'White')
 
+#stapel met kaarten
+transparantkaart1 = pygame.image.load('transparantcard.jpg')
+transparantkaart1 = pygame.transform.scale('transparantcard.jpg', (100, 200))
+
+blauwkaart = pygame.image.load('blauwkaart.jpg')
+blauwkaart = pygame.transform.scale(blauwkaart, (100, 200))
+
+width = blauwkaart.get_rect().width
+height = blauwkaart.get_rect().height
 
 while True:
-    screen.blit(img, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    
+    blauwkaart = pygame.transform.scale(blauwkaart, (100, 200))
+    screen.blit(achtergrond, (0, 0))    
+    screen.blit(blauwkaart, (800, 50))
+    screen.blit(Text,(50, 20))
 
-          
-    pygame.display.update()    
-    #kaarten
+    pygame.display.update()   
+
 
 
 
